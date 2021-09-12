4c1589c0bc8748f720ac14d67ad2b6e19014d8df
Gabriel Dylan <gabriel.dylan@hotmail.com>
2021-09-12 12:24:40 -0300

    First request

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -14,7 +14,6 @@ const Home = () => {
                     <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
                     <Link  className="btn btn-primary btn-lg" to="/dashboard">
                         Acessar dashboard
-                
                     </Link>
                 </div>
             </div>
